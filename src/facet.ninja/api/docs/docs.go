79527a5b8fb2ca46f274c73b5ebb53258154e263
mkotsollaris <mkotsollari@gmail.com>
2020-10-16 00:20:10 -0700

    updated swagger doc

--- src/facet.ninja/api/docs/docs.go
+++ src/facet.ninja/api/docs/docs.go
@@ -4,20 +4,22 @@ import (
 	"facet.ninja/api/domain"
 )
 
-// swagger:route GET /domain Domain domainResponseWrapper
-// Foobar does some amazing stuff.
+// swagger:route GET /domain Domain idOfFoobarEndpoint
+// Get a domain
 // responses:
-//   400: notFoundResponseWrapper
+//	200: okResponseWrapper
+//  400: notFoundResponseWrapper
 
 // swagger:route POST /domain Domain domainResponseWrapper
-// Foobar does some amazing stuff.
+// Create a Domain
 // responses:
-//   200:
+//	200: domainResponseWrapper
+//	400: notFoundResponseWrapper
 
 // swagger:route DELETE /domain Domain domainResponseWrapper
 // Foobar does some amazing stuff.
 // responses:
-//   400
+//	400: notFoundResponseWrapper
 
 // This text will appear as description of your response body.
 // swagger:response domainResponseWrapper
@@ -26,6 +28,23 @@ type domainResponseWrapper struct {
 	Body domain.Domain
 }
 
+// OK
+// swagger:response okResponseWrapper
+type okResponseWrapper struct{}
+
 // Not Found
-// swagger:response NotFoundResponseWrapper
+// swagger:response notFoundResponseWrapper
 type notFoundResponseWrapper struct{}
+
+// swagger:parameters getDomainId
+type domainParamWrapper struct {
+	id string
+}
+
+// swagger:parameters idOfFoobarEndpoint
+type foobarParamsWrapper struct {
+	// This text will appear as description of your request body.
+	// in:query
+	DomainID    string `json:"domainId"`
+	WorkspaceId string `json:"workspaceId"`
+}
