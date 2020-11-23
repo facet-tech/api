71f36e28ccadb95798f9c3e1c41dfc900a4958f3
mkotsollaris <mkotsollari@gmail.com>
2020-11-23 11:59:34 -0800

    user.go cleanup

--- src/facet.ninja/api/user/user.go
+++ src/facet.ninja/api/user/user.go
@@ -1,19 +1,11 @@
 package user
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/base64"
 	"errors"
-	"fmt"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
-	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
-	"sync"
-
 	"facet.ninja/api/db"
 	"facet.ninja/api/util"
 	"github.com/aws/aws-sdk-go/aws"
+	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
@@ -37,33 +29,6 @@ type IdentityMetadata struct {
 	UserPoolID      string
 	AppClientID     string
 	AppClientSecret string
-}
-
-var (
-	identityMetadata IdentityMetadata
-)
-
-func GetIdentityMetadata() IdentityMetadata {
-	var once sync.Once
-	once.Do(func() { //
-		sess := session.Must(session.NewSessionWithOptions(session.Options{
-			SharedConfigState: session.SharedConfigEnable,
-		}))
-		identityMetadata = IdentityMetadata{
-			CognitoClient:   cognito.New(sess),
-			UserPoolID:      "us-west-2_vnM0aVcxD",
-			AppClientID:     "1j7ufkr3pfj82o265ll6iqr6hp",
-			AppClientSecret: "lak12dk2c6knbqjnhln637l23041lfae6hthoh2fqdoj5vn6cjv",
-		}
-	})
-	return identityMetadata
-}
-
-func computeSecretHash(clientSecret string, username string, clientId string) string {
-	mac := hmac.New(sha256.New, []byte(clientSecret))
-	mac.Write([]byte(username + clientId))
-
-	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
 }
 
 func (user *User) fetch() error {
@@ -104,121 +69,6 @@ func (user *User) create() error {
 		_, error = db.Database.PutItem(input)
 	}
 	return error
-}
-
-func (user *User) addUserToUserPool() error {
-	// Initialize a session that the SDK will use to load
-	// credentials from the shared credentials file ~/.aws/credentials.
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-
-	cognitoClient := cognitoidentityprovider.New(sess)
-
-	newUserData := &cognitoidentityprovider.AdminCreateUserInput{
-		DesiredDeliveryMediums: []*string{
-			aws.String("EMAIL"),
-		},
-		UserAttributes: []*cognitoidentityprovider.AttributeType{
-			{
-				Name:  aws.String("email"),
-				Value: aws.String(user.Email),
-			},
-		},
-	}
-
-	// TODO read from env variable
-	newUserData.SetUserPoolId("us-west-2_vnM0aVcxD")
-	newUserData.SetUsername(user.Email)
-
-	res, err := cognitoClient.AdminCreateUser(newUserData)
-	fmt.Println("CHECK", res, "err", err)
-	return err
-}
-
-func (user *User) verify() error {
-	identityMetadata = GetIdentityMetadata()
-
-	const flowUsernamePassword = "USER_PASSWORD_AUTH"
-
-	username := user.Email
-	password := user.Password
-
-	fmt.Println("CHECKAREME", username, password)
-
-	flow := aws.String(flowUsernamePassword)
-	params := map[string]*string{
-		"USERNAME": aws.String(username),
-		"PASSWORD": aws.String(password),
-	}
-
-	if identityMetadata.AppClientSecret != "" {
-		secretHash := computeSecretHash(identityMetadata.AppClientSecret, username, identityMetadata.AppClientID)
-
-		params["SECRET_HASH"] = aws.String(secretHash)
-	}
-
-	authTry := &cognito.InitiateAuthInput{
-		AuthFlow:       flow,
-		AuthParameters: params,
-		ClientId:       aws.String(identityMetadata.AppClientID),
-	}
-
-	res, err := identityMetadata.CognitoClient.InitiateAuth(authTry)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	params["NEW_PASSWORD"] = aws.String(user.Password)
-
-	authChallenge := &cognito.RespondToAuthChallengeInput{
-		ChallengeName:      aws.String(cognito.ChallengeNameTypeNewPasswordRequired),
-		ChallengeResponses: params,
-		ClientId:           aws.String(identityMetadata.AppClientID),
-		Session:            res.Session,
-	}
-
-	fmt.Println("authChallenge", authChallenge)
-
-	req, output := identityMetadata.CognitoClient.RespondToAuthChallenge(authChallenge)
-
-	fmt.Println("req", req, "output: ", output)
-
-	fmt.Println("Final res", res)
-
-	return err
-}
-
-func (user *User) login() *cognito.InitiateAuthOutput {
-	identityMetadata = GetIdentityMetadata()
-
-	const flowUsernamePassword = "USER_PASSWORD_AUTH"
-
-	username := user.Email
-	password := user.Password
-
-	flow := aws.String(flowUsernamePassword)
-	params := map[string]*string{
-		"USERNAME": aws.String(username),
-		"PASSWORD": aws.String(password),
-	}
-
-	if identityMetadata.AppClientSecret != "" {
-		secretHash := computeSecretHash(identityMetadata.AppClientSecret, username, identityMetadata.AppClientID)
-
-		params["SECRET_HASH"] = aws.String(secretHash)
-	}
-
-	authTry := &cognito.InitiateAuthInput{
-		AuthFlow:       flow,
-		AuthParameters: params,
-		ClientId:       aws.String(identityMetadata.AppClientID),
-	}
-
-	res, err := identityMetadata.CognitoClient.InitiateAuth(authTry)
-	fmt.Println("ELA RE res", res, "err", err)
-	return res
 }
 
 func (user *User) delete() error {
