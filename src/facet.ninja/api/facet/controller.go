0d3773bfdaa41eca6f7a9f7f605a030b1c01dabe
andrew-w-pinson <andrew.w.pinson@gmail.com>
2021-01-16 20:15:47 -0800

    Modifying GET /facet to return all facets for a domain if the urlPath… (#72)
    Modifying GET /facet to return all facets for a domain if the urlPath is empty

--- src/facet.ninja/api/facet/controller.go
+++ src/facet.ninja/api/facet/controller.go
@@ -2,6 +2,7 @@ package facet
 
 import (
 	"encoding/json"
+
 	"io/ioutil"
 
 	"facet.ninja/api/util"
@@ -24,8 +25,15 @@ func Get(c *gin.Context) {
 	facet := FacetDTO{}
 	facet.DomainId = c.Request.URL.Query().Get(DOMAIN_ID_QUERY_PARAMATER)
 	facet.UrlPath = c.Request.URL.Query().Get(URL_PATH_QUERY_PARAMATER)
-	error := facet.fetch()
-	util.SetResponseCode(facet, error, c)
+	var facetArray *[]FacetDTO
+	var error error
+	if facet.UrlPath == "" {
+		facetArray, error = FetchAll(facet.DomainId)
+	} else {
+		error = facet.fetch()
+		facetArray = &[]FacetDTO{facet}
+	}
+	util.SetResponseCode(facetArray, error, c)
 }
 
 func Post(c *gin.Context) {
